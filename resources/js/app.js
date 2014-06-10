c6241c8b4e96a895a123020daad90760cc4a2430
Lars Windauer <lars.windauer@betterform.de>
2014-06-10 16:08:25 +0200

    * small enhancements

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,26 @@
 * put application-specific JavaScript code
 */
 'use strict';
+
+jQuery(document).ready(function() {
+    console.log("loaded app.js");
+    $( "#update-feeds" ).click(function() {
+        console.log("executing updates-feeds")
+        var jqxhr = $.ajax( "modules/feed.xql")
+            .done(function() {
+                console.log( "success" );
+            })
+            .fail(function() {
+                console.log( "error" );
+            })
+            .always(function() {
+                console.log( "complete" );
+            });
+        // Perform other work here ...
+        // Set another completion function for the request above
+        // jqxhr.always(function() {
+        //    console.log( "second complete" );
+        // });
+    });
+
+});
